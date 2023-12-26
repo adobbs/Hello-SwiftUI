5d3d10540f4111085a933608dfe97a3113e82386
Andy Dobbs <andrew.dobbs@gmail.com>
2023-12-26 14:42:59 -0500

    Change to text field with a dynamic greeting

--- Hello SwiftUI/ContentView.swift
+++ Hello SwiftUI/ContentView.swift
@@ -8,17 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var name = ""
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, Andy!")
+            TextField("Enter your name here", text: $name)
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                .border(/*@START_MENU_TOKEN@*/Color.gray/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            Text((name == "") ? "Hello!" : "Hello, \(name)!")
                 .font(.title)
                 .foregroundColor(Color.pink)
-            Button("Tap me") {
-                    print("Hello")
-                }
         }
         .padding()
     }

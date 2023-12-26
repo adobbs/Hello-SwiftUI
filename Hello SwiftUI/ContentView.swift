69711adde5b289d54a9e701c79fca8d3d335b7a9
Andy Dobbs <andrew.dobbs@gmail.com>
2023-12-26 14:06:37 -0500

    Initial Commit

---
+++ Hello SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hello SwiftUI
+//
+//  Created by Andy Dobbs on 12/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hello SwiftUI/Hello_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_SwiftUIApp.swift
+//  Hello SwiftUI
+//
+//  Created by Andy Dobbs on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Hello_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
